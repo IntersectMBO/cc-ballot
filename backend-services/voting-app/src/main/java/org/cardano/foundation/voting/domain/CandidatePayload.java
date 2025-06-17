cab07f4ff07658cbe7876ae67f1bb7dfad74902e
Adam Tomaszczyk <ddreporter1124@yahoo.com>
2025-06-17 21:12:19 +0200

    Fix model parsing error

--- backend-services/voting-app/src/main/java/org/cardano/foundation/voting/domain/CandidatePayload.java
+++ backend-services/voting-app/src/main/java/org/cardano/foundation/voting/domain/CandidatePayload.java
@@ -1,19 +1,25 @@
 package org.cardano.foundation.voting.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CandidatePayload {
     private CandidatePayloadData data;
 
     @Data
     @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class CandidatePayloadData {
         private List<Long> votes;
